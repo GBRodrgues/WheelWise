c8ddb0a8d230bb793a61f27929be26785de8a983
GBRodrgues <gabrielcr18@outlook.com>
2025-02-04 19:38:30 -0300

    Padrão Singleton para conexão com banco de dados

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,35 @@
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+
 dotenv.config();
-const user = process.env.DB_USER;
-const pass = process.env.PASS;
-const database = process.env.DATABASE;
-const host = process.env.HOST;
-const sequelize = new Sequelize(database, user , pass, {
-  host: host,
-  dialect: 'postgres',
-  port: '5432',
-  schema: 'public'
-});
-sequelize.authenticate()
-  .then(() => console.log('Conexão bem-sucedida ao banco de dados.'))
-  .catch(err => console.error('Erro ao conectar ao banco:', err));
-export default sequelize;
+
+// Padrão de projeto Singleton para instanciar apenas uma conexão ao banco de dados
+
+class Database {
+  constructor() {
+    if (!Database.instance) {
+      this._sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.PASS, {
+        host: process.env.HOST,
+        dialect: 'postgres',
+        port: 5432,
+        schema: 'public',
+      });
+
+      this._sequelize.authenticate()
+        .then(() => console.log('Conexão bem-sucedida ao banco de dados.'))
+        .catch(err => console.error('Erro ao conectar ao banco:', err));
+
+      Database.instance = this;
+    }
+
+    return Database.instance;
+  }
+
+  getSequelize() {
+    return this._sequelize;
+  }
+}
+
+// Exportando uma única instância
+const databaseInstance = new Database();
+export default databaseInstance.getSequelize();
