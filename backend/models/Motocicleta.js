1c47c38f79553e480660355ff54c21935db079e9
GBRodrgues <gabrielcr18@outlook.com>
2025-02-18 21:06:55 -0300

    inserindo a roda POST /comment

--- backend/models/Motocicleta.js
+++ backend/models/Motocicleta.js
@@ -1,4 +1,4 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import sequelize from '../db.js';
 // Modelo information express (GRASP)
 const Motocicleta = sequelize.define('motocicleta', {
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import sequelize from './db.js';
 import motocicletasRoutes from './routes/motocicletas.js';
 import authRoutes from './routes/auth.js'
+import commentRoutes from "./routes/comments.js"
 
 dotenv.config(); // Carrega variáveis de ambiente do .env
 
@@ -14,6 +15,7 @@ app.use(express.json());
 // Rotas
 app.use('/motocicletas', motocicletasRoutes);
 app.use('/auth', authRoutes)
+app.use('/comment', commentRoutes)
 
 const PORT = process.env.PORT || 3000; // Porta padrão caso não esteja no .env
 
